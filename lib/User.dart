f97dded161e585429aab81dc034342dd2c80ee3c
ParthBadkul <badkul190@gmail.com>
2023-04-05 19:23:28 +0530

    first commit

---
+++ lib/User.dart
@@ -0,0 +1,13 @@
+class User {
+  final int userId;
+  final int Id;
+  final String title;
+  final String body;
+
+  User({
+    required this.Id,
+    required this.body,
+    required this.userId,
+    required this.title,
+  });
+}
